7c3114f40bc25097e598f032ea7cc27a3085049d
emilyyamashita <emilyfumie@gmail.com>
2015-08-18 09:20:20 -0700

    August 18th, 2015 "Hello world demonstration"
    I feel pretty good about this first program. I think that it is a lot
    clearer after doing it for the second time compared to yesterday.

---
+++ HelloWorld2/HelloWorld.java
@@ -0,0 +1,15 @@
+
+/**
+ * This will do a simple println. Our first java code!
+ * 
+ * @author Emily Yamashita 
+ * @version August 18th, 2015
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Emily Yamashita");
+//Display the string
+    }  
+}
